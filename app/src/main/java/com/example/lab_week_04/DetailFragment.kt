fc01dfa76cb497808067e20afb828ab11f7fa32a
DavelDs <163164280+DavelDs@users.noreply.github.com>
2025-09-28 22:58:44 +0700

    Commit No. 2 – Bottom Navigation

--- app/src/main/java/com/example/lab_week_04/DetailFragment.kt
+++ app/src/main/java/com/example/lab_week_04/DetailFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,43 +19,43 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class DetailFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+    private var coffeeId: Int = 0
     private var param1: String? = null
     private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        coffeeId = arguments?.getInt(ListFragment.COFFEE_ID) ?: 0
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_detail, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DetailFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DetailFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        val titleTextView = view.findViewById<TextView>(R.id.coffee_title)
+        val descTextView = view.findViewById<TextView>(R.id.coffee_desc)
+
+        when (coffeeId) {
+            R.id.affogato -> {
+                titleTextView.text = getString(R.string.affogato_title)
+                descTextView.text = getString(R.string.affogato_desc)
             }
+            R.id.americano -> {
+                titleTextView.text = getString(R.string.americano_title)
+                descTextView.text = getString(R.string.americano_desc)
+            }
+            R.id.latte -> {
+                titleTextView.text = getString(R.string.latte_title)
+                descTextView.text = getString(R.string.latte_desc)
+            }
+            else -> {
+                // fallback
+                titleTextView.text = getString(R.string.affogato_title)
+                descTextView.text = getString(R.string.affogato_desc)
+            }
+        }
+        return view
     }
 }
